2f4e34523cf5b32a8ad27cbe37e30d9fa33929a2
ColinMcD05 <colinmcd05@gmail.com>
2026-09-26 13:33:34 -0400

    Fixed Game instance

--- Source/Dissonance_Aria/Private/GameInfo/DAGameInstance.cpp
+++ Source/Dissonance_Aria/Private/GameInfo/DAGameInstance.cpp
@@ -7,3 +7,14 @@ UDAGameInstance::UDAGameInstance()
 {
 	inventory = CreateDefaultSubobject<UInventoryComponent>(TEXT("Inventory"));
 }
+
+void UDAGameInstance::SetInventory(UInventoryComponent* newInventory)
+{
+	for (int i = 0; i < inventory->GetWeaponsLength(); i++)
+	{
+		inventory->AddNewWeapon(*newInventory->GetWeaponAtIndex(i));
+	}
+
+	inventory->SetWeaponOne(newInventory->GetEquippedWeaponOneIndex());
+	inventory->SetWeaponTwo(newInventory->GetEquippedWeaponTwoIndex());
+}
